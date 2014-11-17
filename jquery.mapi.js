760880224d76bbdacc9efcf5fc41e4aac58981b6
Maksim Gusakov <gusakov@milax.com>
2014-11-17 12:33:31 +1000

    1.1.1

--- jquery.mapi.js
+++ jquery.mapi.js
@@ -1,5 +1,5 @@
 /*
- * jQueryMapi v1.1
+ * jQueryMapi v1.1.1
  *
  * Copyright 2014 Milax
  * http://www.milax.com/
@@ -40,9 +40,9 @@ var mapi 					= function ( id ) {
 mapi.ready 					= function ( name ) {
 
 	var providers 				= {
-		yandex						: (typeof ymaps 	== "undefined") ? false : true,
-		google 						: (typeof google 	== "undefined") ? false : true,
-		dblgis 						: (typeof DG 		== "undefined") ? false : true
+		yandex						: ( (typeof ymaps == "undefined") || (typeof ymaps.Map == "undefined") ) ? false : true,
+		google 						: ( (typeof google == "undefined") || (typeof google.maps == "undefined") || (typeof google.maps.Map == "undefined") ) ? false : true,
+		dblgis 						: ( (typeof DG == "undefined") || (typeof DG.map == "undefined") ) ? false : true
 	};
 
 	/* Если запрос шел на конкретного провайдера */
